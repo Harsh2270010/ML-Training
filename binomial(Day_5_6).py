0f9e162d513e40c3992121c3c8136e6410b3a6af
Harsh2270010 <vermaharshkumar990@gmail.com>
2024-11-16 19:31:12 +0530

    Update binomial(Day_5_6).py

--- binomial(Day_5_6).py
+++ binomial(Day_5_6).py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns 
 from scipy.stats import poisson,binom
-n=300 ; p=8.5
+n=300 ; p=0.5
 values=np.arange(1,n+1)
 pmf=binom.pmf(values,n,p)
 #print(pmf)
