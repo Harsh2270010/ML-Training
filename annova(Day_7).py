ec664e391dfb1f02225d898e272efd3b4877e8d8
Harsh2270010 <vermaharshkumar990@gmail.com>
2024-11-18 08:44:32 +0530

    Update annova(Day_7).py

--- annova(Day_7).py
+++ annova(Day_7).py
@@ -4,6 +4,6 @@ from statsmodels.multivariate.manova import MANOVA
 data=pd.read_csv("iris.csv")
 y=data.iloc[:,-1]
 y=y.map({0:'iris.setosa',1:'iris.versicolor',2:'iris.virgincia'})
-manova=MANOVA.from_formula('sepal_length+sepal_width+petal_length+petal_width-species',data)
+manova=MANOVA.from_formula('sepal_length+sepal_width+petal_length+petal_width~Species',data)
 result=manova.mv_test()
 print(result)
